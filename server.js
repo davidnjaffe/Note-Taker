de24c7eb4e6974e8ebf5a72811c5b942cbb8104d
David <Jaffe>
2020-09-28 23:28:41 -0700

    file management to expose server.js to main directory

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 var express = require("express");
+// var path = require('path');
+
 
 var app = express();
 
@@ -6,12 +8,15 @@ var PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("public"));
+app.use(express.static("./Develop/public"));
+// app.use(express.static(path.join(__dirname, 'public')));
 
 
 
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+
+
+require("./Develop/routes/apiRoutes")(app);
+require("./Develop/routes/htmlRoutes")(app);
 
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
